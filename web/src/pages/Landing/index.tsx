acfb86c42c0957bfd1decad4d608bf307067d434
nykolle <malone.nykolle@gmail.com>
2020-08-05 05:29:45 -0300

    home page web view done

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -32,8 +32,8 @@ function Landing() {
                     Estudar
                     </a>
                     <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar" />
-                    Estudar
+                        <img src={giveClassesIcon} alt="Dar aulas" />
+                    Dar aulas
                     </a>
                 </div>
 
