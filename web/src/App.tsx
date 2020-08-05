c2275ddf9bd0774e0233d50de60a2d5adc329581
nykolle <malone.nykolle@gmail.com>
2020-08-05 06:28:32 -0300

    routes created, header component created, pages in progress

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import './assets/styles/global.css'
 
-import Landing from './pages/Landing'
+import Routes from './routes'
 
 function App() {
   return (
-    
-     <Landing />
-    
+    <Routes />
   );
 }
 
