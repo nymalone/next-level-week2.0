6cbd14a66886a33cffa6ad8820e4c965121d71aa
nykolle <malone.nykolle@gmail.com>
2020-08-08 15:08:43 -0300

    useNullAsDefault inserted

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express';
 
 import routes from './routes';
@@ -8,4 +9,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server is running');
+});
